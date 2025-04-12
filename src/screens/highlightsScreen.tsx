cd6b5bdcbb86cb88a7232ce1d2a184690c0db732
shivaraj <shivaraj.feb@gmail.com>
2025-04-12 16:46:09 +0530

    app layout

--- src/screens/highlightsScreen.tsx
+++ src/screens/highlightsScreen.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/screens/highlightsScreen.module.scss";
+import AppLayout from "@/layout/appLayout";
 
 const HighlightScreen = () => {
-    return(
-        <div className={styles.highlightsContainer}>            
-            <h1 className="title">Intellia</h1>
-            <h3 className="font1">test h1 title</h3>
-            <p className="font2">test paragraph description</p>
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+
+  const DrawerContents = (): React.ReactNode => {
+    return (
+        <div className={styles.drawerContent}>
+            <p>content 123</p>
         </div>
     )
-}
+  }
+
+  return (
+    <div className={styles.highlightsContainer}>
+      <AppLayout isDrawerOpen={isDrawerOpen} setIsDrawerOpen={setIsDrawerOpen} drawerComtent = {<DrawerContents/>}>        
+        <h3 className="font1">test h1 title</h3>
+        <p className="font2">test paragraph description</p>
+      </AppLayout>
+    </div>
+  );
+};
+
+
 
 export default React.memo(HighlightScreen);
