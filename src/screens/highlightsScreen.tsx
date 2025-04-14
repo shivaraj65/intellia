8fdfa65e71250b78d0580b6c964e861f8d272868
shivaraj <shivaraj.feb@gmail.com>
2025-04-14 15:22:20 +0530

    linter fixes

--- src/screens/highlightsScreen.tsx
+++ src/screens/highlightsScreen.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "@/styles/screens/highlightsScreen.module.scss";
 import AppLayout from "@/layout/appLayout";
 import { useSelector } from "react-redux";
