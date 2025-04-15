aa6728f0396a92a6a74ca7844dfbf11332c6b88d
shivaraj <shivaraj.feb@gmail.com>
2025-04-16 00:08:03 +0530

    landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 // import Head from "next/head";
 // import Image from "next/image";
+import LandingPage from "@/screens/landing"
 
 export default function Home() {
   return (
     <>
-      index page
+      <LandingPage/>
     </>
   );
 }
